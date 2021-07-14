afc21f3150a82143a3ff8bf29db998fbf9bbf33c
gene poletski <8095687-gene_poletski@users.noreply.gitlab.com>
2021-07-14 15:27:31 +1200

    AWC-1 : Implement simple navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useMachine } from '@xstate/react';
 import controllerMachine,
 { DASHBOARD, SHOPPING_LIST } from './controllerMachine';
+import Navigation from './common/Navigation/Navigation';
 import Dashboard from './pages/Dashboard';
 import ShoppingList from './pages/ShoppingList';
 
 function App(): JSX.Element {
   const [current, send] = useMachine(controllerMachine);
+
+  const handleNavigationClick = useCallback((evt, { name }) => {
+    if (name === 'home') {
+      return send(DASHBOARD);
+    }
+    if (name === 'shoppingList') {
+      return send(SHOPPING_LIST);
+    }
+    return undefined;
+  }, [send]);
+
   return (
     <div className="App">
+      <Navigation onClick={handleNavigationClick} />
       {current.matches(DASHBOARD) && <Dashboard />}
       {current.matches(SHOPPING_LIST) && <ShoppingList />}
     </div>
