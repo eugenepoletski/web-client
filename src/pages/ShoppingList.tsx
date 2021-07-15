938eeaf77008bc51627941a30dff4bcd0cb4aa85
gene poletski <8095687-gene_poletski@users.noreply.gitlab.com>
2021-07-16 09:22:21 +1200

    AWC-1 : Mock Shopping List item data

--- src/pages/ShoppingList.tsx
+++ src/pages/ShoppingList.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ListItem from './shoppingList/ListItem';
 
-const ShoppingList = (): JSX.Element => <h1>Shopping list</h1>;
+type Items = {
+  title: string;
+}[];
+
+const ShoppingList = (): JSX.Element => {
+  const [items, setItems] = useState<Items>([]);
+
+  useEffect(() => {
+    const response = new Promise<Items>((res, rej) => {
+      setTimeout(() => {
+        res([{ title: 'cheese' }, { title: 'eggs' }, { title: 'apples' }]);
+      }, 1000);
+    });
+
+    response.then((_items) => setItems(_items));
+  }, []);
+
+  return (
+    <div>
+      <h1>Shopping list</h1>
+      <ul>
+        {items.map(({ title }) => <ListItem title={title} />)}
+      </ul>
+    </div>
+  );
+};
 
 export default ShoppingList;
